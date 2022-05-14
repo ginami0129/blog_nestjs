cd281453060a837be60b31256d10d826d709c84b
Ginam, Kwon <ginami0129n@naver.com>
2022-05-14 11:17:27 +0900

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import * as Joi from '@hapi/joi';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MYSQL_HOST: Joi.string().required(),
+        MYSQL_PORT: Joi.number().required(),
+        MYSQL_USER: Joi.string().required(),
+        MYSQL_PASSWORD: Joi.string().required(),
+        MYSQL_DB: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
